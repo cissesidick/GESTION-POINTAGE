2be38e2189116404d64296a5b61d1663d8839a24
cissesidick <charlescisse86@gmail.com>
2026-03-10 14:10:54 +0000

    Fix erreur 500 Vercel HTTPS

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,6 +7,6 @@ ALLOWED_HOSTS = ['.vercel.app', 'now.sh', 'localhost', '127.0.0.1']
 
 # Sécurité
 CSRF_TRUSTED_ORIGINS = ['https://*.vercel.app']
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
