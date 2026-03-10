dfc2823dcc3d19ccb522edf3fd9d57fc4fa77077
cissesidick <charlescisse86@gmail.com>
2026-03-10 21:08:57 +0000

    .

--- config/urls.py
+++ config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -9,3 +11,7 @@ urlpatterns = [
     path('pointages/', include('apps.pointages.urls', namespace='pointages')),
     path('', RedirectView.as_view(url='/auth/login/', permanent=False)),
 ]
+
+# Servir les fichiers statiques en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
