f3828fa672888f0c7ca9085e7ff9edb142a9072a
cissesidick <charlescisse86@gmail.com>
2026-03-10 21:24:07 +0000

    .

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,4 +1,16 @@
 from .base import *
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # charge les variables du .env
 
 DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# --- Geofencing ---
+GEOFENCING = {
+    'LATITUDE': float(os.environ.get('ENTREPRISE_LATITUDE', 5.369121)),
+    'LONGITUDE': float(os.environ.get('ENTREPRISE_LONGITUDE', -4.0083)),
+    'RAYON_METRES': float(os.environ.get('ENTREPRISE_RAYON_METRES', 150)),
+    'NOM': os.environ.get('ENTREPRISE_NOM', 'ELITE SAT CANAL PLUS'),
+}
